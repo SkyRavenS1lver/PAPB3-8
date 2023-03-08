63e9039bb1356126d2a5c609103daaa29316eacf
SkyRavenS1lver <101544440+SkyRavenS1lver@users.noreply.github.com>
2023-03-08 14:36:36 +0700

    added change bg and assignment

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,16 +16,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity  implements SensorEventListener {
     private SensorManager sensorManager;
-    private Sensor sensorProx;
-    private Sensor sensorLight;
-    private TextView TVProx;
-    private TextView TVLight;
+    private Sensor sensorProx,sensorLight,sensorAmbient, sensorPressure, sensorHumidity;
+    private TextView TVProx, TVLight, TVAmbient, TVPressure,TVHumidity;
+    ConstraintLayout bg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        bg = findViewById(R.id.BG);
         List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
         StringBuilder sensorText = new StringBuilder();
         for (Sensor sensor:sensorList){sensorText.append(sensor.getName()).append(System.getProperty("line.separator"));}
@@ -31,8 +33,16 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         placeholder.setText(sensorText);
         TVLight = findViewById(R.id.sensor_light);
         TVProx = findViewById(R.id.sensor_prox);
+        TVAmbient = findViewById(R.id.sensor_ambient);
+        TVPressure = findViewById(R.id.sensor_pressure);
+        TVHumidity = findViewById(R.id.sensor_humidity);
         sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         sensorProx = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensorAmbient = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        sensorPressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        sensorHumidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+
+
 
     }
 
@@ -44,6 +54,15 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         }
         if (sensorLight != null){
             sensorManager.registerListener(this,sensorLight,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (sensorPressure != null){
+            sensorManager.registerListener(this,sensorPressure,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (sensorHumidity != null){
+            sensorManager.registerListener(this,sensorHumidity,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (sensorAmbient != null){
+            sensorManager.registerListener(this,sensorAmbient,SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
@@ -60,6 +79,17 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         switch (sensorType){
             case Sensor.TYPE_LIGHT:
                 TVLight.setText(String.format("Light sensor : %1$.2f", currentValue));
+                if (currentValue>20000){bg.setBackgroundColor(Color.RED);}
+                else {bg.setBackgroundColor(Color.BLUE);}
+                break;
+            case Sensor.TYPE_PRESSURE:
+                TVPressure.setText(String.format("Pressure sensor : %1$.2f", currentValue));
+                break;
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                TVAmbient.setText(String.format("AmBient sensor : %1$.2f", currentValue));
+                break;
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                TVHumidity.setText(String.format("Relative Humidity sensor : %1$.2f", currentValue));
                 break;
             case Sensor.TYPE_PROXIMITY:
                 TVProx.setText(String.format("Poximity sensor : %1$.2f", currentValue));
